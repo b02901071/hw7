e7181b0da6c000b527a181ded190a90746517d1e
b02901071 <b02901071@ntu.edu.tw>
2016-11-16 02:17:01 +0800

    eslint

--- api.js
+++ api.js
@@ -2,25 +2,25 @@ import { Router } from 'express';
 
 const router = new Router();
 
-const user_list = {
-	users: [
-		{ avator: 'http://xxx.com', name: 'John', age: 23 },
-		{ avator: 'http://xxx.com', name: 'Amy', age: 18 },
-	]
+const userList = {
+  users: [
+    { avator: 'http://xxx.com', name: 'John', age: 23 },
+    { avator: 'http://xxx.com', name: 'Amy', age: 18 },
+  ],
 };
 
 // Write your restful api here:
 router.get('/users', (req, res) => {
-	res.send(JSON.stringify(user_list));
+  res.send(JSON.stringify(userList));
 });
 
 router.get('/users/:id', (req, res) => {
-	const id = parseInt(req.params.id);
-	if (id === 1 || id === 2) {
-		res.send(JSON.stringify(user_list.users[id - 1]));
-	} else {
-		res.send(JSON.stringify({}));
-	}
+  const id = parseInt(req.params.id, 10);
+  if (id === 1 || id === 2) {
+    res.send(JSON.stringify(userList.users[id - 1]));
+  } else {
+    res.send(JSON.stringify({}));
+  }
 });
 
 
--- src/components/SingleUserPage.js
+++ src/components/SingleUserPage.js
@@ -2,39 +2,38 @@ import React, { Component, PropTypes } from 'react';
 
 
 class SingleUserPage extends Component {
-  constructor(props) {
-	super(props);
-	this.state = {
-	  avatar: '',
-	  name: '',
-	  age: '',
-	};
-  }
-
   static propTypes = {
     id: PropTypes.string.isRequired,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      avatar: '',
+      name: '',
+      age: '',
+    };
+  }
+
   componentDidMount() {
     // fetch `/api/users/${id}` to get user and then set state...
-	function checkStatus(response) {
-	  if (response.status >= 200 && response.status < 300) {
-	    return response;
-	  } else {
-	    var error = new Error(response.statusText);
-	    error.response = response;
-	    throw error;
-	  }
-	}
-	const ref = "/api/users/" + this.props.id.toString();
-	fetch(ref)
+    function checkStatus(response) {
+      if (response.status >= 200 && response.status < 300) {
+        return response;
+      }
+      const error = new Error(response.statusText);
+      error.response = response;
+      throw error;
+    }
+    const ref = `/api/users/${this.props.id.toString()}`;
+    fetch(ref)
       .then(checkStatus)
       .then(response => response.json())
       .then(data => this.setState({
-		  avatar: data.avator,
-		  name: data.name,
-		  age: data.age
-	  }))
+        avatar: data.avator,
+        name: data.name,
+        age: data.age
+      }))
       .catch(err => {
         console.log('request failed', err);
       });
@@ -42,12 +41,12 @@ class SingleUserPage extends Component {
 
   render() {
     return (
-	  <div>
-	    User {this.props.id}
-		<li>Name: {this.state.name}</li>
-		<li>Age: {this.state.age}</li>
-		<li>Avatar: {this.state.avatar}</li>
-	  </div>
+      <div>
+        User {this.props.id}
+        <li>Name: {this.state.name}</li>
+        <li>Age: {this.state.age}</li>
+        <li>Avatar: {this.state.avatar}</li>
+      </div>
     );
   }
 }
